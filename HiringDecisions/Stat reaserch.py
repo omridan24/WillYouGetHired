4fdaaa619a021b51fdf02952a612329d2f3bb0d9
omridan24 <omridan24@gmail.com>
2024-07-25 14:09:49 +0300

    Finished Q 4 ,need to fix P value in Q 2

--- HiringDecisions/Stat reaserch.py
+++ HiringDecisions/Stat reaserch.py
@@ -6,6 +6,8 @@ import seaborn as sns
 from scipy.stats import ttest_1samp
 from scipy.stats import t
 from scipy.stats import chi2
+from scipy.stats import norm
+
 
 
 
@@ -119,6 +121,79 @@ p_value = 1- chi2.cdf(chi_stat_var,amount_of_very_experianced-1)
 #################################
 # Question 3
 #################################
+
+# Calculate the proportion of accepted candidates in all of the sample 
+proportion_accepted = HiringData['HiringDecision'].mean()
+
+#we found that 0.31 were accepted.
+#We are going to check if there is a difference between male and female acceptence rate.
+#Using discritization we will take only female candidates
+#our H0 for acceptence rate of female candidates is 0.31
+p0 = 0.31
+only_female_candidates = HiringData[HiringData['Gender'] == 1]
+proportion_of_female_accepted = only_female_candidates['HiringDecision'].mean()
+number_of_female_tested =len(only_female_candidates)
+#We found that the female acceptence rate is 0.308 in our sample
+#We want to calculate the Z value of the proportion we found
+z_proportion = (proportion_of_female_accepted - p0)/ np.sqrt(p0*(1-p0)/number_of_female_tested)
+#Now we will use the normal distribution table for finding the z value of our significance level
+z_critical_top =norm.ppf(1-alpha/2)
+z_critical_bottom = -z_critical_top
+#we found that the z_proportion of female is in our rejion of acceptence (between (-1.96 and 1.96)) so we except H0
+#Now we will find the p-value for our test 
+p_value_two_tailed = 2 *(norm.cdf(z_proportion))
+
+#we found that our p-value is 0.9504, which is bigger than our significance level 0.95, so we for sure accept our H0
+
+#################################
+# Question 4
+#################################
+
+#In this question we will determine if there is a connection between the recruitment strategy(categorial betweewn 1-3) to the personallity score the candidate recieved.
+#first we will check the mean personallity grades in all of the sample
+mean_personallity_score = HiringData['PersonalityScore'].mean()
+#we found that the mean personallity score in the sample is 49.387
+#our H0 is that there is that there is a connection and that when the interview is aggresive, the personallity score is lower.
+# so we assume that the mu0 in aggresive interviews is going to be 45
+mu0 = 45
+#we will devide them by the strategy and check the personallity score they got
+agrresive_recruitment_strategy = HiringData[HiringData['RecruitmentStrategy']==1]
+moderate_recruitment_strategy = HiringData[HiringData['RecruitmentStrategy']==2]
+conservative_recruitment_strategy = HiringData[HiringData['RecruitmentStrategy']==3]
+
+#let's find the mean of personallity scores within the agressive recruitment group
+mean_personallity_agrresive_recruitment_strategy = agrresive_recruitment_strategy['PersonalityScore'].mean() 
+#we found mean of 49.676 (Higher than General score of the sample!!!!! surprising!!!!!)
+#let's find an estimate to the variance of this group
+unbiased_variance_personallity_when_agressive = np.var(agrresive_recruitment_strategy['PersonalityScore'], ddof=1)
+std_dev_personallity_when_agressive = np.sqrt(unbiased_variance_personallity_when_agressive)
+#let's find the number of people that faced agressive recruitment
+amount_of_agressive_recruitmnent = len(agrresive_recruitment_strategy)
+
+#now let's find the z-value of the statistic
+t_statistic = (mean_personallity_agrresive_recruitment_strategy - mu0) / (std_dev_personallity_when_agressive/np.sqrt(amount_of_agressive_recruitmnent))
+# we found that the z_statistic is 3.33
+#the critical values are now tested using the T distribution
+t_critical_top =t.ppf(1-alpha/2 ,amount_of_agressive_recruitmnent-1 )
+t_critical_bottom = -z_critical_top
+#we found that the critical value is 1.965 which is lower then the t_statistic that we found, so we reject our H0, the Recruitment Strategy does not affect that much the personallity score
+# let's find the  p-value (getting the result we got or bigger/smaller)
+p_value_two_tailed = 2 * (1-t.cdf(abs(t_statistic) ,amount_of_agressive_recruitmnent-1))
+#Our p-value is 0.00009, We reject it for sure because it is smaller the or significance level.
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
 
 
 
